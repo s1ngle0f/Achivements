6f478b1eff08b4e789ac4d9bb6475a186ea3ec3e
s1ngle0f <zubkovmihail12345@gmail.com>
2023-09-01 08:08:56 +0300

    Принятие/отказ ачивки

--- app/src/main/java/com/example/achivements/ui/achivement/AchivementFragment.java
+++ app/src/main/java/com/example/achivements/ui/achivement/AchivementFragment.java
@@ -1,5 +1,9 @@
 package com.example.achivements.ui.achivement;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.achivements.MainActivity;
@@ -20,6 +25,14 @@ import com.example.achivements.models.Achivement;
 import com.example.achivements.models.Comment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import carbon.widget.Button;
 import carbon.widget.ImageView;
 
 /**
@@ -31,6 +44,7 @@ public class AchivementFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final int SELECT_PICTURE = 1;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -68,7 +82,9 @@ public class AchivementFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    private android.widget.ImageView achivementImage;
+    private String imageName;
+    private Uri selectedImageUri;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -81,7 +97,12 @@ public class AchivementFragment extends Fragment {
         TextView achivementStatus = root.findViewById(R.id.achivement_status);
         TextView achivementInputField = root.findViewById(R.id.achivement_inputfield_comment);
         ImageView achivementsSender = root.findViewById(R.id.achivement_send_button);
+        achivementImage = root.findViewById(R.id.achivement_image);
         RecyclerView achivementCommentRV = root.findViewById(R.id.achivement_comment_rv);
+        Button cancelButton = root.findViewById(R.id.achivement_cancel_button);
+        Button acceptButton = root.findViewById(R.id.achivement_accept_button);
+        android.widget.LinearLayout inputComment = root.findViewById(R.id.achivement_input_comment);
+        android.widget.LinearLayout selector = root.findViewById(R.id.achivement_selector);
 
         achivementCommentRV.setLayoutManager(new LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false));
         CommentAdapter commentAdapter = new CommentAdapter();
@@ -90,7 +111,93 @@ public class AchivementFragment extends Fragment {
         achivementCommentRV.setAdapter(commentAdapter);
 
         if(MainActivity.BottomNV != null) MainActivity.BottomNV.setVisibility(View.GONE);
-        if(MainActivity.user == null) root.findViewById(R.id.achivement_input_comment).setVisibility(View.GONE);
+        if(MainActivity.user == null) {
+            inputComment.setVisibility(View.GONE);
+            selector.setVisibility(View.GONE);
+        }
+        else{
+            if(achivement.getImage() != null){
+                String projectFolderPath = MainActivity.mainActivity.getApplicationContext().getFilesDir() + "/server/";
+                File projectFolder = new File(projectFolderPath);
+                if (!projectFolder.exists()) {
+                    projectFolder.mkdir();
+                }
+
+                File avatarImageFile = new File(projectFolderPath + achivement.getImage());
+                achivementImage.setImageURI(Uri.fromFile(avatarImageFile));
+            }
+            if(achivement.getUser() == MainActivity.user && achivement.getStatus() == Achivement.Status.ACTIVE){
+                inputComment.setVisibility(View.GONE);
+                selector.setVisibility(View.VISIBLE);
+                achivementImage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent,
+                                "Select Picture"), SELECT_PICTURE);
+                    }
+                });
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        achivement.setStatus(Achivement.Status.FAILED);
+                        MainActivity.ServerEmulator.EditUser(achivement.getUser());
+                    }
+                });
+                acceptButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(selectedImageUri != null){
+                            try {
+                                // Copy the selected image to the project folder
+                                String projectFolderPath = MainActivity.mainActivity.getApplicationContext().getFilesDir() + "/server/";
+                                File projectFolder = new File(projectFolderPath);
+                                if (!projectFolder.exists()) {
+                                    projectFolder.mkdir();
+                                }
+
+                                String imageName = new File("" + selectedImageUri).getName(); // Change the image name as needed
+                                File avatarImageFile = new File(projectFolderPath + imageName);
+
+                                InputStream inputStream = MainActivity.mainActivity.getContentResolver().openInputStream(selectedImageUri);
+                                OutputStream outputStream = new FileOutputStream(avatarImageFile);
+
+                                byte[] buffer = new byte[1024];
+                                int length;
+                                while ((length = inputStream.read(buffer)) > 0) {
+                                    outputStream.write(buffer, 0, length);
+                                }
+
+                                inputStream.close();
+                                outputStream.close();
+
+                                // Update user's avatarImage and set the ImageView
+                                achivement.setImage(imageName);
+                                achivement.setStatus(Achivement.Status.COMPLETED);
+                                MainActivity.ServerEmulator.EditUser(achivement.getUser());
+                            } catch (FileNotFoundException e) {
+                                throw new RuntimeException(e);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                });
+            }else{
+                inputComment.setVisibility(View.VISIBLE);
+                selector.setVisibility(View.GONE);
+                if(achivement.getImage() != null) {
+                    String projectFolderPath = MainActivity.mainActivity.getApplicationContext().getFilesDir() + "/server/";
+                    File avatarImageFile = new File(projectFolderPath + achivement.getImage());
+                    if (avatarImageFile.exists())
+                        achivementImage.setImageURI(Uri.fromFile(avatarImageFile));
+                }
+            }
+        }
         achivementText.setText(achivement.getText());
         achivementLogin.setText(achivement.getUser().getLogin());
         achivementStatus.setText("Achivement: " + achivement.getStatusText());
@@ -111,5 +218,19 @@ public class AchivementFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+//                Uri selectedImageUri = data.getData();
+//                MainActivity.user.setAvatarImage(selectedImageUri);
+//                avatarAccount.setImageURI(selectedImageUri);
+                selectedImageUri = data.getData();
+                achivementImage.setImageURI(selectedImageUri);
+            }
+        }
     }
 }
